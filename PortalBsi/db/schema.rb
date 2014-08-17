24779537c754a44fe3040b090485310cc10ed69f
HIRAN <hiran.tassinari@gmail.com>
2014-08-17 17:00:52 -0300

    Criação da pagina de professores, faltando fechar a troca dos links para as image tags

--- PortalBsi/db/schema.rb
+++ PortalBsi/db/schema.rb
@@ -13,6 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20140817045018) do
 
+  create_table "tccs", force: true do |t|
+    t.string   "titulo"
+    t.string   "resumo"
+    t.date     "data"
+    t.string   "orientador"
+    t.string   "local"
+    t.string   "coorientador"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -24,26 +35,11 @@ ActiveRecord::Schema.define(version: 20140817045018) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-
-  end
-
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-end
-
-ActiveRecord::Schema.define(version: 20140815173352) do
-
-  create_table "tccs", force: true do |t|
-    t.string   "titulo"
-    t.string   "resumo"
-    t.date     "data"
-    t.string   "orientador"
-    t.string   "local"
-    t.string   "coorientador"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
