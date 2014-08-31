b3811ac85f187a3ce949d88a6bf4e2f4ebcb7cd5
HIRAN <hiran.tassinari@gmail.com>
2014-08-31 17:11:31 -0300

    Criado o mailer de TCC e o envio quando um Tcc é criado dentro do sistema Close #3

---
+++ PortalBsi/app/mailers/tcc_mailer.rb
@@ -0,0 +1,12 @@
+class TccMailer < ActionMailer::Base
+  default from: "sistema@uniriotec.br"
+
+  def create_email(tcc)
+    @tcc = tcc
+    mail({
+             to: "bsi-alunos@uniriotec.br",
+             bcc: "hiran.tassinari@gmail.com",
+             subject: "Novo Tcc criado"
+         })
+  end
+end
