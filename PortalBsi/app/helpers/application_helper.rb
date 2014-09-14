66d3994493ebda45fefe76e0f999372966af4a9b
Lanchinho <guilherme.albuquerque@uniriotec.br>
2014-09-14 02:10:49 -0300

    Algumas mudanças no form de registrar agas de estagio. Adição de helper para msgs de erro e personalização das msgs de erro no css.

--- PortalBsi/app/helpers/application_helper.rb
+++ PortalBsi/app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       return "ativo"
     end
   end
+
+  def error_tag(model, attribute)
+  	if model.errors.has_key? attribute
+  	  content_tag(:div,model.errors[attribute].first, class: 'error_message')
+  	end
+  end
 end
--- PortalBsi/app/models/oportunidade.rb
+++ PortalBsi/app/models/oportunidade.rb
@@ -1,5 +1,5 @@
 class Oportunidade < ActiveRecord::Base
-  validates_presence_of :company_name, :title, :description, :expiration_date, :has_partnership
+  validates_presence_of :company_name, :title, :description, :expiration_date
   validates_length_of :description, minimum: 30, allow_blank: false
   validates_length_of :company_name, minimum: 3, allow_blank: false
   validates_length_of :title, minimum: 10, allow_blank: false
