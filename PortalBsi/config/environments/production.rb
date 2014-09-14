67f52665aa865b771c44336bf2e82b04af65d019
HIRAN <hiran.tassinari@gmail.com>
2014-09-14 14:55:02 -0300

    SMTP DO GOOGLE

--- PortalBsi/config/environments/production.rb
+++ PortalBsi/config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'sistemabsi',
+      password:             'rubyonrails',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 end
