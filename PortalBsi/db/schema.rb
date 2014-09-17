7342c6afa8ee15263afe6ad17b0cb446a86fb0e8
HIRAN <hiran.tassinari@gmail.com>
2014-09-16 21:55:50 -0300

    atualização do TO-DO

--- PortalBsi/db/schema.rb
+++ PortalBsi/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912040958) do
+ActiveRecord::Schema.define(version: 20140915201917) do
 
   create_table "tags", force: true do |t|
     t.string   "nome"
@@ -57,13 +57,15 @@ ActiveRecord::Schema.define(version: 20140912040958) do
     t.string   "matricula"
     t.string   "facebook_link"
     t.string   "github_link"
-    t.integer  "users_id"
     t.integer  "tccs_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["tccs_id"], name: "index_users_on_tccs_id"
-  add_index "users", ["users_id"], name: "index_users_on_users_id"
 
 end
