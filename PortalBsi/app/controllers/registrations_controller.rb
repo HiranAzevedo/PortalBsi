133e3039e2ac1841fd08989f4b9c45cd8183bf64
Thiago Leao <thiagobleao@gmail.com>
2014-09-18 23:57:25 -0300

    Altera página de perfil

--- PortalBsi/app/controllers/registrations_controller.rb
+++ PortalBsi/app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def account_update_params
-    params.require(:user).permit(:nome, :apelido, :matricula, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:nome, :apelido, :matricula, :email, :password, :password_confirmation, :current_password, :avatar)
   end
 
 end
--- PortalBsi/app/models/user.rb
+++ PortalBsi/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_one :tcc
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "//placehold.it/100"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
