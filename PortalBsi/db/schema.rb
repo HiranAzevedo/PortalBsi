62eb390005a453df8dc6db0e58f1ff10c997aecb
HIRAN <hiran.tassinari@gmail.com>
2014-10-16 15:11:58 -0300

    acerto do TO-DO

--- PortalBsi/db/schema.rb
+++ PortalBsi/db/schema.rb
@@ -118,7 +118,8 @@ ActiveRecord::Schema.define(version: 20141007195827) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: trueß
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["tccs_id"], name: "index_users_on_tccs_id"
   add_index "users", ["turma_id"], name: "index_users_on_turma_id"
 
 end
