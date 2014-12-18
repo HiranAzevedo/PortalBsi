6c35e8a7812bafe597faef7f0f10080d22744687
Thiago Leao <thiagobleao@gmail.com>
2014-12-18 01:04:29 -0200

    Mais commit antes da defesa

--- PortalBsi/app/helpers/application_helper.rb
+++ PortalBsi/app/helpers/application_helper.rb
@@ -41,7 +41,7 @@ module ApplicationHelper
 
   def flash_class(level)
     case level
-      when :notice then "alert alert-info"
+      when :notice then "alert alert-success"
       when :success then "alert alert-success"
       when :error then "alert alert-danger"
       when :alert then "alert alert-danger"
